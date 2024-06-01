2940c9b5446360b48febe9953812350a1d510ccd
Akbar_Safarov <akbarsafarov915@gmail.com>
2024-06-01 08:36:59 +0500

    home page first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-
-import './App.css'
+import React from "react";
+import "./App.css";
+import HomePage from "./Components/HomePage/HomePage";
+import logo from "./Components/HomePage/Icons/logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1>hello world</h1>
-    </>
-  )
+    <div className="home">
+      <div className="container">
+        <nav className="navbar">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="list">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#guru">Guru</a>
+            <a href="#sisva">Siswa</a>
+            <a href="#pendaftaran">Pendaf</a>
+            <a href="#lainya">Lainya</a>
+            <a href="#kontak">Kontak</a>
+          </div>
+        </nav>
+        <HomePage />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="box">
+        <h3>Motto</h3>
+        <h1>
+          SMP N 1 Cibadak (Cerdas Beretika) Ceria, Empati, Rasional, Damai,
+          Aktif, Sabar, Bersih, Elok, Tulus, Iman, Konsiste, Amanah.
+        </h1>
+        <div className="btn">Contact us</div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
